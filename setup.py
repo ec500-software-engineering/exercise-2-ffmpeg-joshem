dfb51d2ad90235283208576585be7bf54fcb7404
joshem <joshe@bu.edu>
2019-02-25 18:03:11 -0500

    add setup.py'

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='exercise-2-ffmpeg-joshem',
+    version='',
+    url='',
+    license='MIT',
+    author='joshemb',
+    author_email='joshe@bu.edu',
+    description='exercise 2',
+    install_requires=['flake8','pytest'],
+)
